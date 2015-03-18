867c4ea046b16b8242deda3bbf4c71a36940129d
Carlo Nicora <carlo.nicora@gmail.com>
2015-03-18 11:30:21 +0000

    structureController abstracted, slimmed, updated and commented

--- src/structureController.php
+++ src/structureController.php
@@ -21,125 +21,145 @@
  */
 namespace CarloNicora\cryogen;
 
-class structureController{
-    public function readStructure(){
-        return(false);
-    }
+/**
+ * The structureController class provides the structure that database specific structure controllers will implement.
+ * Moreover, it provides a method to generate the database object files from the DB structure
+ */
+abstract class structureController{
+    /**
+     * Returns the structure of all the tables in the connected database
+     *
+     * @return array
+     */
+    public abstract function readStructure();
 
-    public function readTableStructure($tableName){
-        unset($tableName);
-        return(false);
-    }
+    /**
+     * Read the structure of a table from the database and returns the metaTable object
+     *
+     * @param $tableName
+     * @return metaTable
+     */
+    public abstract function readTableStructure($tableName);
 
-    public function createView($viewSql){
-        unset($viewSql);
-        return(false);
-    }
+    /**
+     * Creates a view based on the specified sql code
+     *
+     * @param $viewSql
+     * @return bool
+     */
+    public abstract function createView($viewSql);
 
-    public function createTable($metaTable, $isFederated = FALSE, $federatedLink = NULL){
-        unset($metaTable);
-        unset($isFederated);
-        unset($federatedLink);
-        return(false);
-    }
+    /**
+     * Creates a table on the database using the meta table passed as parameter
+     *
+     * @param metaTable $metaTable
+     * @param bool $isFederated
+     * @param string $federatedLink
+     * @return bool
+     */
+    public abstract function createTable(metaTable $metaTable, $isFederated=false, $federatedLink=null);
 
-    public function updateTable($metaTable){
-        unset($metaTable);
-        return(false);
-    }
+    /**
+     * Updates a table on the database using the meta table passed as parameter
+     *
+     * @param metaTable $metaTable
+     * @return bool
+     */
+    public abstract function updateTable(metaTable $metaTable);
 
-    public function generatePersistencyFiles($metaTables){
-        $returnValue = "&lt;?php
-";
-        foreach ($metaTables as $metaTable){
-            $returnValue .= "class " . $metaTable->name . " extends entity{\r\n";
-            $returnValue .= "	protected \$_initialValues;\r\n\r\n";
-            if (sizeof($metaTable->fields) > 0){
-                foreach ($metaTable->fields as $field){
-                    $returnValue .= "	public \$" . $field->name . ";\r\n";
-                }
-                $returnValue .= "\r\n";
+    /**
+     * Generates the database object to use in the PHP code for the specified table
+     *
+     * @param metaTable $metaTable
+     * @return string
+     */
+    public function generatePersistencyFiles(metaTable $metaTable){
+        $returnValue = "class " . $metaTable->name . " extends entity{\r\n";
+        $returnValue .= "	protected \$_initialValues;\r\n\r\n";
+        if (sizeof($metaTable->fields) > 0){
+            foreach ($metaTable->fields as $field){
+                $returnValue .= "	public \$" . $field->name . ";\r\n";
             }
-            if (sizeof($metaTable->relations) > 0){
-                foreach ($metaTable->relations as $relation){
-                    if (substr($relation->target, strlen($relation->target)-5) == "List_"){
-                        $returnValue .= "	public \$" . $relation->target . $relation->linkedField . "_" . ";\r\n";
-                    } else {
-                        $returnValue .= "	public \$" . $relation->target . $relation->field . "_" . ";\r\n";
-                    }
-                }
-                $returnValue .= "\r\n";
-            }
-            $returnValue .= "	public static \$table;\r\n";
-            if (sizeof($metaTable->fields) > 0){
-                foreach ($metaTable->fields as $field){
-                    $returnValue .= "	public static \$field_" . $field->name . ";\r\n";
-                }
-                $returnValue .= "\r\n";
-            }
-            if (sizeof($metaTable->relations) > 0){
-                foreach ($metaTable->relations as $relation){
-                    if (substr($relation->target, strlen($relation->target)-5) == "List_"){
-                        $returnValue .= "	public static \$relation_" . $relation->target . $relation->linkedField . ";\r\n";
-                    } else {
-                        $returnValue .= "	public static \$relation_" . $relation->target . $relation->field . ";\r\n";
-                    }
-                }
-                $returnValue .= "\r\n";
-            }
-            $additionalParams = '';
-            $additionalConstruct = '';
-
-            if (sizeof($metaTable->fields) > 0){
-                $additionalConstruct .= "\r\n";
-                foreach ($metaTable->fields as $field){
-                    $additionalParams .= ", \$" . $field->name . " = NULL";
-                    $additionalConstruct .= "		if (isset(\$" . $field->name . ")) \$this->" . $field->name . " = \$" . $field->name . ";\r\n";
+            $returnValue .= "\r\n";
+        }
+        if (sizeof($metaTable->relations) > 0){
+            foreach ($metaTable->relations as $relation){
+                if (substr($relation->target, strlen($relation->target)-5) == "List_"){
+                    $returnValue .= "	public \$" . $relation->target . $relation->linkedField . "_" . ";\r\n";
+                } else {
+                    $returnValue .= "	public \$" . $relation->target . $relation->field . "_" . ";\r\n";
                 }
             }
-
-            $returnValue .= "	public function __construct(\$entity = NULL" . $additionalParams . "){\r\n";
-            $returnValue .= "		\$this->metaTable = self::\$table;\r\n";
-            $returnValue .= "		\$this->_initialValues = [];\r\n";
-            $returnValue .= "		\r\n";
-            $returnValue .= "		parent::__construct(\$entity);\r\n";
-            $returnValue .= $additionalConstruct;
-            $returnValue .= "	}\r\n";
-            $returnValue .= "}\r\n";
-
-            $returnValue .= $metaTable->name . "::\$table = new metaTable(\"" . $metaTable->name . "\", \"" . $metaTable->name . "\");\r\n";
-            if (sizeof($metaTable->fields) > 0){
-                foreach ($metaTable->fields as $field){
-                    if (!isset($field->size) || $field->size == ""){
-                        $field->size = 0;
-                    }
-                    $returnValue .= $metaTable->name . "::\$field_" . $field->name . " = new metaField(" . $field->position . ", \"" . $field->name . "\", \"" . $field->type . "\", " . $field->size . ", " . ($field->isPrimaryKey ? "TRUE" : "FALSE") . ", " . ($field->isAutoNumbering ? "TRUE" : "FALSE") . ");\r\n";
-                }
-                foreach ($metaTable->fields as $field){
-                    $returnValue .= $metaTable->name . "::\$table->fields[] = " . $metaTable->name . "::\$field_" . $field->name . ";\r\n";
-                }
-                $returnValue .= "\r\n";
+            $returnValue .= "\r\n";
+        }
+        $returnValue .= "	public static \$table;\r\n";
+        if (sizeof($metaTable->fields) > 0){
+            foreach ($metaTable->fields as $field){
+                $returnValue .= "	public static \$field_" . $field->name . ";\r\n";
             }
+            $returnValue .= "\r\n";
+        }
+        if (sizeof($metaTable->relations) > 0){
+            foreach ($metaTable->relations as $relation){
+                if (substr($relation->target, strlen($relation->target)-5) == "List_"){
+                    $returnValue .= "	public static \$relation_" . $relation->target . $relation->linkedField . ";\r\n";
+                } else {
+                    $returnValue .= "	public static \$relation_" . $relation->target . $relation->field . ";\r\n";
+                }
+            }
+            $returnValue .= "\r\n";
+        }
+        $additionalParams = '';
+        $additionalConstruct = '';
 
-            if (sizeof($metaTable->relations) > 0){
-                foreach ($metaTable->relations as $relation){
-                    if (substr($relation->target, strlen($relation->target)-5) == "List_"){
-                        $returnValue .= $metaTable->name . "::\$relation_" . $relation->target . $relation->linkedField . " = new metaRelation(\"" . $relation->target . $relation->linkedField . "_" . "\", \"field_" . $relation->field . "\", \"" . $relation->table . "\", \"field_" . $relation->linkedField . "\", \"" . $relation->linkedTable . "\", " . $relation->relationType . ");\r\n";
-                    } else {
-                        $returnValue .= $metaTable->name . "::\$relation_" . $relation->target . $relation->field . " = new metaRelation(\"" . $relation->target . $relation->field . "_" . "\", \"field_" . $relation->field . "\", \"" . $relation->table . "\", \"field_" . $relation->linkedField . "\", \"" . $relation->linkedTable . "\", " . $relation->relationType . ");\r\n";
-                    }
-                }
-                foreach ($metaTable->relations as $relation){
-                    if (substr($relation->target, strlen($relation->target)-5) == "List_"){
-                        $returnValue .= $metaTable->name . "::\$table->relations[] = " . $metaTable->name . "::\$relation_" . $relation->target . $relation->linkedField . ";\r\n";
-                    } else {
-                        $returnValue .= $metaTable->name . "::\$table->relations[] = " . $metaTable->name . "::\$relation_" . $relation->target . $relation->field . ";\r\n";
-                    }
-                }
-                $returnValue .= "\r\n";
+        if (sizeof($metaTable->fields) > 0){
+            $additionalConstruct .= "\r\n";
+            foreach ($metaTable->fields as $field){
+                $additionalParams .= ", \$" . $field->name . "=null";
+                $additionalConstruct .= "		if (isset(\$" . $field->name . ")) \$this->" . $field->name . " = \$" . $field->name . ";\r\n";
             }
         }
-        $returnValue .= "?&gt;";
+
+        $returnValue .= "	public function __construct(\$entity=null" . $additionalParams . "){\r\n";
+        $returnValue .= "		\$this->metaTable = self::\$table;\r\n";
+        $returnValue .= "		\$this->_initialValues = [];\r\n";
+        $returnValue .= "		\r\n";
+        $returnValue .= "		parent::__construct(\$entity);\r\n";
+        $returnValue .= $additionalConstruct;
+        $returnValue .= "	}\r\n";
+        $returnValue .= "}\r\n";
+
+        $returnValue .= $metaTable->name . "::\$table = new metaTable(\"" . $metaTable->name . "\", \"" . $metaTable->name . "\");\r\n";
+        if (sizeof($metaTable->fields) > 0){
+            foreach ($metaTable->fields as $field){
+                if (!isset($field->size) || $field->size == ""){
+                    $field->size = 0;
+                }
+                $returnValue .= $metaTable->name . "::\$field_" . $field->name . " = new metaField(" . $field->position . ", '" . $field->name . "', '" . $field->type . "', '" . $field->size . "', " . ($field->isPrimaryKey ? "true" : "false") . ", " . ($field->isAutoNumbering ? "true" : "false") . ");\r\n";
+            }
+            foreach ($metaTable->fields as $field){
+                $returnValue .= $metaTable->name . "::\$table->fields[] = " . $metaTable->name . "::\$field_" . $field->name . ";\r\n";
+            }
+            $returnValue .= "\r\n";
+        }
+
+        if (sizeof($metaTable->relations) > 0){
+            foreach ($metaTable->relations as $relation){
+                if (substr($relation->target, strlen($relation->target)-5) == "List_"){
+                    $returnValue .= $metaTable->name . "::\$relation_" . $relation->target . $relation->linkedField . " = new metaRelation('" . $relation->target . $relation->linkedField . "_" . "', 'field_" . $relation->field . "', '" . $relation->table . "', 'field_" . $relation->linkedField . "', '" . $relation->linkedTable . "', " . $relation->relationType . ");\r\n";
+                } else {
+                    $returnValue .= $metaTable->name . "::\$relation_" . $relation->target . $relation->field . " = new metaRelation('" . $relation->target . $relation->field . "_" . "', 'field_" . $relation->field . "', '" . $relation->table . "', 'field_" . $relation->linkedField . "', '" . $relation->linkedTable . "', " . $relation->relationType . ");\r\n";
+                }
+            }
+            foreach ($metaTable->relations as $relation){
+                if (substr($relation->target, strlen($relation->target)-5) == "List_"){
+                    $returnValue .= $metaTable->name . "::\$table->relations[] = " . $metaTable->name . "::\$relation_" . $relation->target . $relation->linkedField . ";\r\n";
+                } else {
+                    $returnValue .= $metaTable->name . "::\$table->relations[] = " . $metaTable->name . "::\$relation_" . $relation->target . $relation->field . ";\r\n";
+                }
+            }
+            $returnValue .= "\r\n";
+        }
         return($returnValue);
     }
 }
